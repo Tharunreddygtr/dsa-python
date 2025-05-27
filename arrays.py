1fe0f9800242e540788b7e61b2b0b59092b18bd3
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2025-05-27 19:58:19 +0530

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -202,3 +202,37 @@ print(find_peak_in_bitonic_array_with_duplicates([7, 7, 7, 7, 7, 100, 7, 7, 7]))
 # Output: 100
 
 
+
+
+def find_first_and_last_index(arr, target):
+    def find_first():
+        left, right = 0, len(arr) - 1
+        first = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                first = mid
+                right = mid - 1  # keep going left
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return first
+
+    def find_last():
+        left, right = 0, len(arr) - 1
+        last = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                last = mid
+                left = mid + 1  # keep going right
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return last
+
+    return find_first(), find_last()
+
+print(find_first_and_last_index([1,1,1,3,4,5,6], 1))
